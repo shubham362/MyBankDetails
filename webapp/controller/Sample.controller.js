3263f3a479917e8628b653889863c19306a4a317
Shubham Kshirsagar <shubhamkshirsagar770@gmail.com>
2024-09-13 17:46:34 +0000

    first commit

---
+++ webapp/controller/Sample.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+      
+], function(Controller ) {
+    'use strict';
+    
+    return Controller.extend("shubham.project1.controller.Sample", {
+        onInit: function() {
+             
+        }, 
+      });
+});
