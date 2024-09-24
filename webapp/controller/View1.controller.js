42d0999d48f65732c3e7acafd4ff9faa26869d92
Shubham Kshirsagar <shubhamkshirsagar770@gmail.com>
2024-09-24 10:20:36 +0000

    Updates for i18

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,87 +1,107 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-    
+
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("shubham.project1.controller.View1", {
-        onInit: function () {
-            
-            let OData = {
-                "accountdetails": {
-                    "accountnum": "00000000009",
-                    "name": "Techtutsindia",
-                    "ifsc": "0000001",
-                    "customerid": "12345",
-                    "address": {
-                        "city": "Manhattan ",
-                        "postalcode": "000000",
-                        "housenumber": "001",
-                        "street": "Wall Street",
-                        "state": "New York",
-                        "country": "USA"
-                    }
-                },
-                "carddetails": [
-                    {
-                        "cardtype": "debit card",
-                        "cardnumber": "0000000000001",
-                        "cardcompany": "Visa card",
-                        "assignmentdate": "assigned on 23 Aug 2017",
-                        "state": true
+        return Controller.extend("shubham.project1.controller.View1", {
+            onInit: function () {
+                alert("this is onInit Block")
+                debugger
+
+
+                let OData = {
+                    "accountdetails": {
+                        "accountnum": "00000000009",
+                        "name": "Techtutsindia",
+                        "ifsc": "0000001",
+                        "customerid": "12345",
+                        "address": {
+                            "city": "Manhattan ",
+                            "postalcode": "000000",
+                            "housenumber": "001",
+                            "street": "Wall Street",
+                            "state": "New York",
+                            "country": "USA"
+                        }
                     },
-                    {
-                        "cardtype": "credit card",
-                        "cardnumber": "0000000000003",
-                        "cardcompany": "master card",
-                        "assignmentdate": "assigned on 20 Oct 2020",
-                        "state": false
-                    },
-                    {
-                        "cardtype": "debit card",
-                        "cardnumber": "0000000000002",
-                        "cardcompany": "master card",
-                        "assignmentdate": "assigned on 23 Aug 2017",
-                        "state": true
-                    }
-                ]
-                
-            };
-           
-            let oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData(OData);
-            this.getView().setModel(oModel,"oBankDetails");
+                    "carddetails": [
+                        {
+                            "cardtype": "debit card",
+                            "cardnumber": "0000000000001",
+                            "cardcompany": "Visa card",
+                            "assignmentdate": "assigned on 23 Aug 2017",
+                            "state": true
+                        },
+                        {
+                            "cardtype": "credit card",
+                            "cardnumber": "0000000000003",
+                            "cardcompany": "master card",
+                            "assignmentdate": "assigned on 20 Oct 2020",
+                            "state": false
+                        },
+                        {
+                            "cardtype": "debit card",
+                            "cardnumber": "0000000000002",
+                            "cardcompany": "master card",
+                            "assignmentdate": "assigned on 23 Aug 2017",
+                            "state": true
+                        }
+                    ]
+
+                };
+
+                let oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData(OData);
+                this.getView().setModel(oModel, "oBankDetails");
 
 
-            
-        },
 
-        tap:function(){
-            this.getOwnerComponent().getRouter().navTo("RouteView2");
-        }
-        
-         
+            },
 
-        // onReadData:function() {
-        //     var ODataModel =   this.getownerComponent().getModel();
-        //     ODataModel.read("/Products",{
-        //       success:function(O){
-        //           debugger;
-        //       },
-        //       errot:function(oEvent){
-        //           debugger;
-        //       }
-        //     });
+            tap: function () {
+                this.getOwnerComponent().getRouter().navTo("RouteView2");
+            },
 
-        //   }
-            
+            onOpenBankDetails: function () {
+                if (!this.MoreBankDetail) {
+                    this.MoreBankDetail = this.loadFragment(
+                        { name: "shubham.project1.view.fragments.MoreBankDetail" }
+                    );
+                }
+                this.MoreBankDetail.then(
+                    function (ODialog) {
+                        ODialog.open();
 
-            
-            
-            
-           
-        
-        
+                    });
+            },
+            onCloseBankDetails: function () {
+                this.byId("MoreBankDetail").close();
+
+            }
+
+
+
+            // onReadData:function() {
+            //     var ODataModel =   this.getownerComponent().getModel();
+            //     ODataModel.read("/Products",{
+            //       success:function(O){
+            //           debugger;
+            //       },
+            //       errot:function(oEvent){
+            //           debugger;
+            //       }
+            //     });
+
+            //   }
+
+
+
+
+
+
+
+
+        });
     });
-});
